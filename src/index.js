b5047513834d6e80c53363b8131dc68dff98e589
acmesfun <77114076+acmesfun@users.noreply.github.com>
2025-07-01 09:28:31 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.closeai.me": dockerHub,
-  "quay.closeai.me": "https://quay.io",
-  "gcr.closeai.me": "https://gcr.io",
-  "k8s-gcr.closeai.me": "https://k8s.gcr.io",
-  "k8s.closeai.me": "https://registry.k8s.io",
-  "ghcr.closeai.me": "https://ghcr.io",
-  "cloudsmith.closeai.me": "https://docker.cloudsmith.io",
+  "docker.rjl.cc": dockerHub,
+  "quay.rjl.cc": "https://quay.io",
+  "gcr.rjl.cc": "https://gcr.io",
+  "k8s-gcr.rjl.cc": "https://k8s.gcr.io",
+  "k8s.rjl.cc": "https://registry.k8s.io",
+  "ghcr.rjl.cc": "https://ghcr.io",
+  "cloudsmith.rjl.cc": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.closeai.me": dockerHub,
+  "docker-staging.rjl.cc": dockerHub,
 };
 
 function routeByHosts(host) {
